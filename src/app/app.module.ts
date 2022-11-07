c1abd764f8df07c4c80438590ce054ffb973df5c
Santiago L Martinez <santiago.martinez@visma.com>
2022-11-07 19:23:31 -0300

    Primer commit, empezando front con Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Header/header.component';
+import { SliderComponent } from './Slider/slider.component';
+import { PaymentContainerComponent } from './payment-container/payment-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SliderComponent,
+    PaymentContainerComponent
   ],
   imports: [
     BrowserModule,
